8c53ce8ba54cc17fafe5fe19ff76bb38fa2d3da9
Lauzifu <lau.zifu@gmail.com>
2024-04-08 17:36:30 +1000

    Update Dashboard design

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -4,7 +4,12 @@ const projectPanel = document.querySelector("#projects");
 
 newButton.addEventListener("click", ()=>{
     let project = document.createElement("div");
+    let title = document.createElement("h2");
+    let content = document.createElement("p");
+
     project.classList.add("project_card");
+    project.appendChild(title);
+    project.appendChild(content);
     projectPanel.appendChild(project);
 });
 
