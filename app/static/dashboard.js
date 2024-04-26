4db5f7ce3c67cf48dbf2e446e9e6685b794f7bad
Lauzifu <lau.zifu@gmail.com>
2024-04-26 14:12:25 +1000

    Add project creation

--- app/static/dashboard.js
+++ app/static/dashboard.js
@@ -1,15 +1,40 @@
 
 const newButton = document.querySelector("#new");
+const closeButton = document.querySelector("#closeButton");
 const projectPanel = document.querySelector("#projects");
+const createButton = document.querySelector("#submitButton");
+const projectForm = document.querySelector("#myForm");
 
 newButton.addEventListener("click", ()=>{
-    let project = document.createElement("div");
-    let title = document.createElement("h2");
-    let content = document.createElement("p");
-
-    project.classList.add("project_card");
-    project.appendChild(title);
-    project.appendChild(content);
-    projectPanel.appendChild(project);
+    projectForm.style.display = "block";
 });
+
+closeButton.addEventListener("click", ()=>{
+    projectForm.style.display = "none";
+    document.querySelector("#project_description").value= "";
+    document.querySelector("#project_title").value = "";
+})
+
+createButton.addEventListener("click", ()=>{
+    let description = document.createElement("p");
+    let projectTitle = document.createElement("h1");
+    let projectCard = document.createElement("form");
+    let enter = document.createElement("button");
+    
+    enter.type = "submit";
+    enter.textContent = "Enter Project";
+    description.textContent = document.querySelector("#project_description").value;
+    projectTitle.textContent = document.querySelector("#project_title").value;
+    
+    projectCard.method = "post";
+    projectCard.classList.add("project_card");
+    projectCard.appendChild(projectTitle);
+    projectCard.appendChild(description);
+    projectCard.appendChild(enter);
+    projectPanel.appendChild(projectCard);
+
+    projectForm.style.display = "none";
+    document.querySelector("#project_description").value= "";
+    document.querySelector("#project_title").value = "";
+})
 
